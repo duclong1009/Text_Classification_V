d138c4ca64cb4f172bf1b7570ef9f6f707428529
duclong1009 <66026651+duclong1009@users.noreply.github.com>
2021-06-11 09:45:15 +0700

    add eval

--- main.py
+++ main.py
@@ -45,7 +45,15 @@ def main(arg):
                 i + 1, arg.epochs, loss, accuracy
             )
         )
-        es(accuracy, model, optimizer)
+        # es(accuracy, model, optimizer)
+
+    test_df = pd.read_excel(arg.fig_root + "/news.xlsx")
+    test_df, _ = train_test_split(test_df, train_size=0.005)
+    test_dataset = BertDataset(test_df, tokenizer, arg.max_len, vncore_tokenizer)
+    test_dataloder = DataLoader(test_dataset, arg.batch_size, shuffle=False)
+    output_test, target_test = eval_fn(test_dataloder, model, device)
+    test_acc = sum(np.array(output_test) == np.array(target_test)) / len(target_test)
+    print("Accuracy test: ", test_acc)
 
 
 if __name__ == "__main__":
