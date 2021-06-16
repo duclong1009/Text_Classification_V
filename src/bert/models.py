f5e70c6eaa4449328ee4a2a2527295f6d09f70a7
duclong1009 <66026651+duclong1009@users.noreply.github.com>
2021-06-16 23:55:32 +0700

    add fc_bert

--- src/bert/models.py
+++ src/bert/models.py
@@ -82,3 +82,53 @@ class GRU_BERT(nn.Module):
         output, hidden_state = self.gru(output)
         output = output[:, -1, :]
         return self.softmax(self.fc(output))
+
+
+class FC_BERT(nn.Module):
+    def __init__(self, bert_model, n_classes, dropout, n_segments):
+        super(GRU_BERT, self).__init__()
+        config = AutoConfig.from_pretrained(
+            bert_model,
+            from_tf=False,
+            # output_hidden_states=True,
+        )
+        self.size_token_embed = config.hidden_size
+        self.bert = AutoModel.from_pretrained(bert_model, config=config)
+        self.bert_drop = nn.Dropout(dropout)
+        self.fc = nn.Linear(
+            in_features=config.hidden_size * n_segments, out_features=n_classes
+        )
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        content_input_ids, content_attention_mask, content_token_type_ids = (
+            x["input_ids"],
+            x["attention_mask"],
+            x["token_type_ids"],
+        )
+        n_sentences, max_segments, segment_length = content_input_ids.size()
+        total_segment = n_sentences * max_segments
+
+        content_input_ids = content_input_ids.view(total_segment, segment_length)
+        content_attention_mask = content_attention_mask.view(
+            total_segment, segment_length
+        )
+        content_token_type_ids = content_token_type_ids.view(
+            total_segment, segment_length
+        )
+
+        out = self.bert(
+            content_input_ids,
+            attention_mask=content_attention_mask,
+            token_type_ids=content_token_type_ids,
+        )
+        output = out.last_hidden_state
+        output = self.bert_drop(output)
+        output = output[:, 0, :]
+        output = output.view(n_sentences, max_segments, self.size_token_embed)
+        output = output.view(n_sentences, -1)
+        # output = torch.mean(output, dim=1)
+
+        # output, hidden_state = self.gru(output)
+        # output = output[:, -1, :]
+        return self.softmax(self.fc(output))
